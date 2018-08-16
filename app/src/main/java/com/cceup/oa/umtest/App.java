42a4a6a4829a1dec71e8aa7cb60ffe4e456fa531
mengxiangyang <18811373023@163.com>
2018-08-16 17:51:56 +0800

    UM推送服务

---
+++ app/src/main/java/com/cceup/oa/umtest/App.java
@@ -0,0 +1,19 @@
+package com.cceup.oa.umtest;
+
+import android.app.Application;
+
+/**
+ * 姓名：孟向阳
+ * 公司：中能联合工程技术有限公司
+ * 邮箱：1171134249@qq.com
+ * 时间：2018/5/8
+ * 描述：
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
